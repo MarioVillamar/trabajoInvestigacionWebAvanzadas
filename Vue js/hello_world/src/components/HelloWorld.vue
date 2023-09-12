f020b9d0ce923ff18a18c3f192b4ca6001d241a3
marioVillamar <mjvc98@hotmail.es>
2023-09-12 11:10:20 -0500

    Hello world in react and Vue

---
+++ Vue js/hello_world/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h2>Hola mundo desde Vue js</h2>
+  <p>Autor: Mario Villamar </p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
